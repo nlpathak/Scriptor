395c1a64074d62652dc257cecdb10d81c338427d
Sabeel Mansuri <smansuri@ucsd.edu>
2019-05-10 00:32:36 -0700

    init react app

---
+++ scriptor-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar.js';
+
+function App() {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ scriptor-app/src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import './Navbar.css';
+
+class Navbar extends Component {
+    render(){
+        // if user notlogged in
+        return(
+            <div class="navbar">
+                <div class="logo">SCRIPTOR</div>
+                <ul>
+                    <li><a href="#login">LOG IN</a></li>
+                    <li><a href="#signup">SIGN UP</a></li>
+                    <li><a href="#about">ABOUT</a></li>
+                </ul>
+            </div>
+        );
+        // if logged in
+        /*return(
+            <div class="navbar">
+                <div class="logo">SCRIPTOR</div>
+                <ul>
+                    <li><a href="#login">LOG OUT</a></li>
+                    <li><a href="#signup">FAVORITES</a></li>
+                    <li><a href="#signup">HISTORY</a></li>
+                    <li><a href="#about">ABOUT</a></li>
+                </ul>
+            </div>
+        );*/
+    }
+}
+
+export default Navbar;
