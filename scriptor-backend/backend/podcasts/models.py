51582587fb75817fd602b15d73a0c346ef6d5f3c
subby <subby8pie@gmail.com>
2019-05-20 12:41:05 -0700

    Added a script to download the metadata and audio files for a given podcast.

--- scriptor-backend/backend/podcasts/models.py
+++ scriptor-backend/backend/podcasts/models.py
@@ -1,25 +1,25 @@
-from elasticsearch_dsl import Document, Text, Date
+from elasticsearch_dsl import Document, Text, Date, Integer
 
 
 class Podcast(Document):
     """
     This defines a Podcast model
     """
-    title = Text()
 
-    # Some metadata
-    department = Text()
+    # Podcast metadata
+    title = Text()
+    lecture_num = Integer()
+    date = Date()
+    ucsd_podcast_video_url = Text()
+    ucsd_podcast_audio_url = Text()
+    full_transcript = Text()
+
+    # Some course metadata
     course_num = Text()
+    department = Text()
     quarter = Text()
     professor = Text()
     section_id = Text()
-
-    date = Date()
-
-    ucsd_podcast_video_url = Text()
-    ucsd_podcast_audio_url = Text()
-
-    full_transcript = Text()
 
     # Elasticsearch index settings
     class Index:
--- scriptor-backend/scripts/download_podcast_data.py
+++ scriptor-backend/scripts/download_podcast_data.py
@@ -1,5 +1,139 @@
-# TODO: This is an interactive script that we can run to selectively import the course podcasts that we need
-# It will get a list of all the podcasts with audio, get all of their metadata, & download their audio files to a directory
-# so that speech to text can be run on them
+import datetime
+import json
+import os
+import urllib.request
 
-course_podcast_website_url = input("Course podcast website URL: ")
+import furl
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+PODCAST_AUDIO_VERSION = 0
+PODCAST_VIDEO_VERSION = 1
+
+
+class InvalidPodcastError(Exception):
+    pass
+
+
+class NoPodcastAudioError(Exception):
+    pass
+
+
+def extract_podcast_data(podcast_num, course_podcast_url):
+    result = requests.get(course_podcast_url, params={"l": podcast_num, "v": PODCAST_AUDIO_VERSION})
+    content = result.content
+
+    soup = BeautifulSoup(content, features="html.parser")
+
+    if "No recordings found" in soup.text:
+        raise InvalidPodcastError()
+
+    professor = soup.find("span", id="author_Label").text.strip()
+    quarter = soup.find("span", id="quarter_Label").text.strip()
+
+    lecture_header = soup.find("div", id="LectureHeader").h2.text.strip()
+
+    lecture_num = int(lecture_header[len("Lecture "):lecture_header.index(",")].strip())
+    date = lecture_header[lecture_header.index(",") + 1:].strip()
+
+    date = datetime.datetime.strptime(date, '%m/%d/%Y')
+
+    audio_url = soup.noscript.div.audio.get("src")
+    if not audio_url:
+        raise NoPodcastAudioError()
+
+    # Extract the video URL
+    result = requests.get(course_podcast_url, params={"l": podcast_num, "v": PODCAST_VIDEO_VERSION})
+    content = result.content
+    soup = BeautifulSoup(content, features="html.parser")
+
+    video_url = soup.noscript.div.video.get("src") or ""
+    return {
+        "lecture_num": lecture_num,
+        "date": date.strftime('%m/%d/%Y'),
+        "audio_url": audio_url,
+        "video_url": video_url,
+        "professor": professor,
+        "quarter": quarter
+    }
+
+
+def download_podcast_data(course_podcast_website_url, course_name, department, course_num, section_id):
+    f = furl.furl(course_podcast_website_url)
+    f.remove(['l', 'v'])
+    course_podcast_website_url = f.url
+
+    podcast_course_metadata = {
+        "course_name": course_name,
+        "department": department,
+        "course_num": course_num,
+        "section_id": section_id
+    }
+
+    course_podcasts = []
+
+    current_podcast_num = 0
+
+    print("Scraping podcast metadata...")
+
+    while True:
+        current_podcast_num += 1
+        try:
+            podcast_data = extract_podcast_data(current_podcast_num, course_podcast_website_url)
+            podcast_data.update(podcast_course_metadata)
+            course_podcasts.append(podcast_data)
+        except NoPodcastAudioError:
+            # This podcast has no audio, so skip to the next one
+            continue
+        except InvalidPodcastError:
+            # We've reached the end of the list of podcasts for this course
+            break
+
+    if not course_podcasts:
+        print("No podcasts found.")
+        return
+
+    professor = course_podcasts[0]['professor']
+    quarter = course_podcasts[0]['quarter']
+
+    # Download audio files for this list of course podcasts
+    course_podcast_dir = os.path.join("./fixtures", "podcasts",
+                                      f"{course_name} - {section_id} - {professor} - {quarter}")
+    os.makedirs(course_podcast_dir)
+
+    course_podcast_audio_dir = os.path.join(course_podcast_dir, "audios")
+    os.makedirs(course_podcast_audio_dir)
+
+    print(f"Downloading podcast audio files to {course_podcast_audio_dir}...")
+    for course_podcast in tqdm(course_podcasts):
+        audio_file_url = course_podcast["audio_url"]
+        lecture_num = course_podcast["lecture_num"]
+
+        audio_filepath = os.path.join(course_podcast_dir, "audios", f"lecture_{lecture_num}.mp3")
+        urllib.request.urlretrieve(audio_file_url, audio_filepath)
+
+    # Compile the list of course podcast metadata into a json file
+    course_podcast_metadata_json_filepath = os.path.join(course_podcast_dir, "metadata.json")
+    course_podcast_metadata = {
+        "course_podcasts": course_podcasts,
+        "num_podcasts": len(course_podcasts)
+    }
+
+    json.dump(course_podcast_metadata, open(course_podcast_metadata_json_filepath, "w+"), indent=4)
+    print(f"Saved metadata file to {course_podcast_metadata_json_filepath}.")
+
+
+if __name__ == "__main__":
+    while True:
+        course_podcast_website_url = input("course podcast website URL: ").strip()
+        course_name = input("course name: ").strip()
+        department = input("department (e.g. CSE, MAE, etc.): ").strip()
+        course_num = int(input("course num: ").strip())
+        section_id = input("section id: ").strip()
+
+        print("\n")
+
+        download_podcast_data(course_podcast_website_url, course_name, department, course_num, section_id)
+
+        print("\n\n")
