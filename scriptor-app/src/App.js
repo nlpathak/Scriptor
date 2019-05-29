6829622e90b81edc580dde8a64ce4b8da41d67e2
Sabeel Mansuri <smansuri@ucsd.edu>
2019-05-28 17:12:51 -0700

    add 404 page

--- scriptor-app/src/App.js
+++ scriptor-app/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/Navbar.js';
 import Home from './Home.js';
 import About from './About.js';
@@ -11,6 +11,7 @@ import './App.css';
 import Results from './Results.js';
 import Forgotpass from './Forgotpass.js';
 import Transcript from './Transcript.js';
+import NotFound from './NotFound.js';
 
 class App extends Component {
     render() {
@@ -18,15 +19,18 @@ class App extends Component {
                 <Router>
                     <Navbar/>
                     <div>
-                        <Route exact path="/" component={Home}/>
-                        <Route path="/about" component={About}/>
-                        <Route path="/user" component={User}/>
-                        <Route path="/forgot_password" component={Forgotpass}/>
-                        <Route path="/results" component={Results}/>
-                        <Route path="/history" component={History}/>
-                        <Route path="/favorites" component={Favorites}/>
-                        <Route path="/podcast" component={PodcastPage}/>
-                        <Route path="/transcript" component={Transcript}/>
+                        <Switch>
+                            <Route exact path="/" component={Home}/>
+                            <Route path="/about" component={About}/>
+                            <Route path="/user" component={User}/>
+                            <Route path="/forgot_password" component={Forgotpass}/>
+                            <Route path="/results" component={Results}/>
+                            <Route path="/history" component={History}/>
+                            <Route path="/favorites" component={Favorites}/>
+                            <Route path="/podcast" component={PodcastPage}/>
+                            <Route path="/transcript" component={Transcript}/>
+                            <Route path="*" component={NotFound} />
+                        </Switch>
                     </div>
                 </Router>
         );
---
+++ scriptor-app/src/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NotFound.css';
+
+function NotFound() {
+  return (
+    <div className='notfound'>
+        <h1>Uh oh! Something went wrong.</h1>
+        <h5>Let's get you home:</h5>
+        <button>Home</button>
+    </div>
+  );
+}
+
+export default NotFound;
