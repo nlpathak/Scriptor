7c2cf93a7ca912b9f789b85347ff354c1c3e9741
ashukla45 <ayshukla@ucsd.edu>
2019-06-02 14:34:08 -0700

    build

---
+++ scriptor-app/build/precache-manifest.8e5c9fec3524e9997dac74c8a37bf748.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8034935aec244213b02ecbb1d710f02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9946f60c5bafb49909de",
+    "url": "/static/css/2.0b329a55.chunk.css"
+  },
+  {
+    "revision": "c9927f22ba4ba40d73a4",
+    "url": "/static/css/main.1ee0801a.chunk.css"
+  },
+  {
+    "revision": "9946f60c5bafb49909de",
+    "url": "/static/js/2.b62cb974.chunk.js"
+  },
+  {
+    "revision": "c9927f22ba4ba40d73a4",
+    "url": "/static/js/main.fd0f89d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d78368918e806a09d141ef48374baa11",
+    "url": "/static/media/Amithab.d7836891.png"
+  },
+  {
+    "revision": "86aa5df892c6a088853049e0ec7c8c37",
+    "url": "/static/media/Ayush.86aa5df8.png"
+  },
+  {
+    "revision": "b3506a212acab83ac85f48be435788f8",
+    "url": "/static/media/David.b3506a21.png"
+  },
+  {
+    "revision": "9cdaa2b9d0afdd9c60d49710982d82d8",
+    "url": "/static/media/Kelvin.9cdaa2b9.png"
+  },
+  {
+    "revision": "9a9562ef00133c5b9324b5c62188f63c",
+    "url": "/static/media/Nikhil.9a9562ef.png"
+  },
+  {
+    "revision": "5310381d1cae42b0aea45894b5a39871",
+    "url": "/static/media/Pranav.5310381d.png"
+  },
+  {
+    "revision": "6309c087f84b584a4c4a505c538d41de",
+    "url": "/static/media/Raghavan.6309c087.png"
+  },
+  {
+    "revision": "8bad89e585608b510e1fae4cc4ff4c0f",
+    "url": "/static/media/Sabeel.8bad89e5.png"
+  },
+  {
+    "revision": "0c85894f3a5ef22e729b83a56edd58d7",
+    "url": "/static/media/ScriptorLogo.0c85894f.jpg"
+  },
+  {
+    "revision": "8ed332dbf39884e1532d37d0523e43a5",
+    "url": "/static/media/Shuu.8ed332db.png"
+  },
+  {
+    "revision": "25a042f50135e0263afd526654343fed",
+    "url": "/static/media/Som.25a042f5.png"
+  },
+  {
+    "revision": "31ee9661d4c85ca4346a05d73c5911a3",
+    "url": "/static/media/Subhash.31ee9661.png"
+  },
+  {
+    "revision": "1668d9ae52832e5800c1fbae949aa443",
+    "url": "/static/media/starOff.1668d9ae.png"
+  },
+  {
+    "revision": "298685c0a8ab8d503040ce8604635f3c",
+    "url": "/static/media/starOn.298685c0.png"
+  }
+]);
